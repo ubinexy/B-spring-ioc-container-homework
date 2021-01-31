ab9b108d0ec5bd0d83f11250a0e3f9a0da82e975
shiqi <shiqi.tong@thoughtworks.com>
2021-01-31 15:20:01 +0800

    方法1: controller 单例, service prototype, 使用ScopedProxyMode

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,16 +1,19 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @RestController
 public class GreetingController {
 
     private final GreetingService greetingService;
 
-    @Autowired
     public GreetingController(GreetingService greetingService) {
+        System.out.println("Creating controller");
         this.greetingService = greetingService;
     }
 
