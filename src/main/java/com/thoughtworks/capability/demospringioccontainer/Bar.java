2660bb46bd7bd86fe010da9a56d128149cf2f35d
shiqi <shiqi.tong@thoughtworks.com>
2021-01-31 16:02:16 +0800

    方法3：使用postConstruct注解，消除循环依赖

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -3,14 +3,17 @@ package com.thoughtworks.capability.demospringioccontainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class Bar {
 
+    @Autowired
     private Foo foo;
 
-    @Autowired
-    public void setFoo(Foo foo) {
-        this.foo = foo;
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
     }
 
     public void hi() {
